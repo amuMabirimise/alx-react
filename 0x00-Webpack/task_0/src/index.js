042240ced45bb707bbc5dcb30421331426e5f3b6
amuMabirimise <awmabirimise@gmail.com>
2024-04-02 20:57:32 +0200

    setup environment

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+// Use jQuery to add elements to the body
+$(document).ready(function() {
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
+});
