6804ed224462ea633811ac7dc60b5e3fcffcaafd
amuMabirimise <awmabirimise@gmail.com>
2024-04-05 18:00:00 +0200

    readme

---
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import HolbertonLogo from './HolbertonLogo.png';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={HolbertonLogo} className="App-logo" alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <footer className="App-footer">
+        <p>{getFooterCopy(true)}</p>
+        <p>Copyright {getFullYear()} - {getFooterCopy(false)}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
