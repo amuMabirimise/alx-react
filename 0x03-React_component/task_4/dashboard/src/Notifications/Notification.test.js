4b8a5d625dd7985f14cd70267fd470984e6e140d
amuMabirimise <awmabirimise@gmail.com>
2024-04-10 18:12:53 +0200

    adding last test

--- 0x03-React_component/task_4/dashboard/src/Notifications/Notification.test.js
+++ 0x03-React_component/task_4/dashboard/src/Notifications/Notification.test.js
@@ -1,40 +1,34 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Notifications from "./Notifications";
-import NotificationItem from "./NotificationItem";
 
-describe("Notification component tests", () => {
-  it("renders Notification component without crashing", () => {
-    const notification = shallow(<Notifications />);
-
-    expect(notification).toBeDefined();
-  });
-
-  it("renders ul", () => {
-    const notification = shallow(<Notifications />);
-
-    expect(notification.find("ul")).toBeDefined();
-  });
-
-  it("renders correct number of NotificationItem components", () => {
+describe("Notifications component tests", () => {
+  it("does not rerender when props are updated with the same list", () => {
     const listNotifications = [
       { id: 1, type: "default", value: "New course available" },
       { id: 2, type: "urgent", value: "New resume available" },
-      { id: 3, type: "urgent", html: { __html: "test" } }
     ];
-    const notification = shallow(<Notifications listNotifications={listNotifications} />);
 
-    expect(notification.find(NotificationItem)).toHaveLength(3);
+    const wrapper = shallow(<Notifications listNotifications={listNotifications} />);
+    const instance = wrapper.instance();
+    const shouldUpdate = instance.shouldComponentUpdate({ listNotifications });
+
+    expect(shouldUpdate).toBe(false);
   });
 
-  it("calls markAsRead function when NotificationItem is clicked", () => {
-    const listNotifications = [{ id: 1, type: "default", value: "New course available" }];
-    const markAsRead = jest.fn();
-    const notification = shallow(
-      <Notifications listNotifications={listNotifications} />
-    );
+  it("rerenders when props are updated with a longer list", () => {
+    const listNotifications1 = [
+      { id: 1, type: "default", value: "New course available" },
+    ];
+    const listNotifications2 = [
+      { id: 1, type: "default", value: "New course available" },
+      { id: 2, type: "urgent", value: "New resume available" },
+    ];
 
-    notification.find(NotificationItem).simulate("click");
-    expect(markAsRead).toHaveBeenCalledWith(1);
+    const wrapper = shallow(<Notifications listNotifications={listNotifications1} />);
+    const instance = wrapper.instance();
+    const shouldUpdate = instance.shouldComponentUpdate({ listNotifications: listNotifications2 });
+
+    expect(shouldUpdate).toBe(true);
   });
 });
--- 0x03-React_component/task_4/dashboard/src/Notifications/Notifications.js
+++ 0x03-React_component/task_4/dashboard/src/Notifications/Notifications.js
@@ -1,23 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Notifications.css";
-import closeIcon from "../assets/close-icon.png";
 import NotificationItem from "./NotificationItem";
-import PropTypes from "prop-types";
 import NotificationItemShape from "./NotificationItemShape";
+import PropTypes from "prop-types";
 
-class Notifications extends React.Component {
-  constructor(props) {
-    super(props);
-    this.markAsRead = this.markAsRead.bind(this);
-  }
-
-  markAsRead(id) {
-    console.log(`Notification ${id} has been marked as read`);
+class Notifications extends Component {
+  shouldComponentUpdate(nextProps) {
+    return nextProps.listNotifications.length > this.props.listNotifications.length;
   }
 
   render() {
     const { displayDrawer, listNotifications } = this.props;
-
     return (
       <React.Fragment>
         {displayDrawer ? (
@@ -27,35 +20,13 @@ class Notifications extends React.Component {
             </div>
             <div className="Notifications">
               <ul>
-                {listNotifications && listNotifications.length > 0 ? (
+                {listNotifications.length > 0 ? (
                   listNotifications.map(({ id, html, type, value }) => (
-                    <React.Fragment key={id}>
-                      <NotificationItem
-                        id={id}
-                        type={type}
-                        value={value}
-                        html={html}
-                        markAsRead={this.markAsRead}
-                      />
-                      <button
-                        style={{ color: "#3a3a3a", fontWeight: "bold", background: "none", border: "none", fontSize: "10px", position: "absolute", right: "2px", top: "2px", cursor: "pointer" }}
-                        aria-label="Close"
-                        onClick={() => console.log("Close button has been clicked")}
-                      >
-                        <img src={closeIcon} alt="closeIcon" width="10px" />
-                      </button>
-                    </React.Fragment>
+                    <NotificationItem key={id} type={type} value={value} html={html} />
                   ))
                 ) : (
                   <div className="">
                     <NotificationItem value="No new notification for now" />
-                    <button
-                      style={{ color: "#3a3a3a", fontWeight: "bold", background: "none", border: "none", fontSize: "10px", position: "absolute", right: "2px", top: "2px", cursor: "pointer" }}
-                      aria-label="Close"
-                      onClick={() => console.log("Close button has been clicked")}
-                    >
-                      <img src={closeIcon} alt="closeIcon" width="10px" />
-                    </button>
                   </div>
                 )}
               </ul>
