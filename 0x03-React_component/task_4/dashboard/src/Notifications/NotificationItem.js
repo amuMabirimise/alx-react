5707b6045e52987e0d1683d36bf8b2d79dee5ec2
amuMabirimise <awmabirimise@gmail.com>
2024-04-10 18:07:47 +0200

    declaring pure components

--- 0x03-React_component/task_4/dashboard/src/Notifications/NotificationItem.js
+++ 0x03-React_component/task_4/dashboard/src/Notifications/NotificationItem.js
@@ -1,22 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Notifications.css";
 import PropTypes from "prop-types";
 
-class NotificationItem extends Component {
-  render() {
-    const { type, value, html, markAsRead, id } = this.props;
-    return (
-      <>
-        {type && value ? (
-          <li onClick={() => markAsRead(id)} data-notification-type={type}>
-            {value}
-          </li>
-        ) : null}
-        {html ? <li onClick={() => markAsRead(id)} data-urgent dangerouslySetInnerHTML={{ __html: html }}></li> : null}
-      </>
-    );
-  }
-}
+const NotificationItem = React.memo(({ type, value, html, markAsRead, id }) => {
+  return (
+    <>
+      {type && value ? (
+        <li onClick={() => markAsRead(id)} data-notification-type={type}>
+          {value}
+        </li>
+      ) : null}
+      {html ? (
+        <li onClick={() => markAsRead(id)} data-urgent dangerouslySetInnerHTML={{ __html: html }}></li>
+      ) : null}
+    </>
+  );
+});
 
 NotificationItem.propTypes = {
   type: PropTypes.string.isRequired,
