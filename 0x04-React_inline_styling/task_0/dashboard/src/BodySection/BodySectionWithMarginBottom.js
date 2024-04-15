f3e1a66c0c3eba5399d10a3805596c43a6a5614b
amuMabirimise <awmabirimise@gmail.com>
2024-04-15 21:03:30 +0200

    inline styling

---
+++ 0x04-React_inline_styling/task_0/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+import BodySection from "./BodySection";
+import "./BodySectionWithMarginBottom.css";
+
+const BodySectionWithMarginBottom = ({ title, children }) => (
+  <div className="bodySectionWithMargin">
+    <BodySection title={title}>{children}</BodySection>
+  </div>
+);
+
+BodySectionWithMarginBottom.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default BodySectionWithMarginBottom;
