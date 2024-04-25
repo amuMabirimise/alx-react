169a5143c166ea563f933d4c692ceee69f9cf636
amuMabirimise <awmabirimise@gmail.com>
2024-04-25 08:38:55 +0200

    Lazy Seq

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,24 +1,18 @@
-import { Seq } from 'immutable';
+const { Seq } = require('immutable');
 
-export default function printBestStudents(object) {
+function printBestStudents(object) {
   const seq = Seq(object);
 
-  const filtered = seq.filter((student) => {
-    student.firstName.charAt(0).toUpperCase();
-    return student.score > 70;
-  });
-
-  function capFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
+  const filtered = seq.filter((student) => student.score > 70)
+                      .map((student) => ({
+                        ...student,
+                        firstName: student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
+                        lastName: student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1)
+                      }));
 
   const JSObject = filtered.toJS();
 
-  Object.keys(JSObject).map((key) => {
-    JSObject[key].firstName = capFirstLetter(JSObject[key].firstName);
-    JSObject[key].lastName = capFirstLetter(JSObject[key].lastName);
-    return JSObject[key];
-  });
-
   console.log(JSObject);
 }
+
+module.exports = printBestStudents;
